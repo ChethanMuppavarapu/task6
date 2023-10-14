c6ed5128fe858c4f10d906bb8bffb6c3a6ef225b
ChethanMuppavarapu <chethanmuppavarapu30@gmail.com>
2023-10-14 22:54:26 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Hi My name is chethan.I want to be a Data Science in Ai and Ml.I am pusuing a 3rd Year in Vellore institute of technology
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="chethanmuppavarapu30@gmail.com">Email</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin " target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/dashboard " target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
+
